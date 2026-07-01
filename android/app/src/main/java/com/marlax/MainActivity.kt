3d7640b4a311254b4e082995f447ce79ad0880be
Naila-Akram <naila.akram@ibex.co>
2026-07-01 16:46:54 +0500

    android's control bttons removed

--- android/app/src/main/java/com/marlax/MainActivity.kt
+++ android/app/src/main/java/com/marlax/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.marlax
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +23,24 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableImmersiveSticky()
+  }
+
+  override fun onWindowFocusChanged(hasFocus: Boolean) {
+    super.onWindowFocusChanged(hasFocus)
+    // Re-hide the bar after it transiently reappears (dialogs, keyboards, permission prompts…).
+    if (hasFocus) enableImmersiveSticky()
+  }
+
+  private fun enableImmersiveSticky() {
+    val controller = WindowCompat.getInsetsController(window, window.decorView)
+    // "Sticky": an edge swipe reveals the bar briefly, then it auto-hides again.
+    controller.systemBarsBehavior =
+        WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    // Hide only the nav (back / recents / home) bar. Use Type.systemBars() to hide the status bar too.
+    controller.hide(WindowInsetsCompat.Type.navigationBars())
+  }
 }
